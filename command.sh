b32bee6611f22694d453564a2e8ae87536392849
labicquette <komete17@gmail.com>
2024-03-15 18:48:47 +0100

    loading models

--- command.sh
+++ command.sh
@@ -1,5 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python3 legal_eval_ft.py --data-path ./data/distilbert/recette \
-                                                --model-name almanach/camembert-base \
-                                                --output-dir ./models/camembert-base-less-naive-weights-/ \
-                                                --custom-trainer \
-                                                --num-epochs 10 
+CUDA_VISIBLE_DEVICES=1 python3 legal_eval_infer.py ./data/distilbert/recette/test.csv \
+        --model-name ./models/camembert-base-less-naive-weights-/almanach_camembert-base-ft-BUILD/checkpoint-1200   
